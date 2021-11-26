4dc0b58b99080de32349b6840c81c5415c6e3ae6
Hanawaro Kato <hanawaro3@gmail.com>
2021-11-26 16:19:21 +0300

    set bootstrap 4.6 npm version make layout page (footer + header + content)

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="{{ asset('css/main.css') }}">
 
     <title>@yield('title')</title>
@@ -13,9 +14,132 @@
 </head>
 <body>
 
-@yield('content')
+<nav class="navbar navbar-dark py-0 border-bottom">
+    <div class="container d-flex align-content-between dropdown"  id="dropdownMenuButton" >
+        <a class="navbar-brand logo" href="#">GameInfo</a>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+        <div class="d-flex align-items-center ">
+            <a href="#">
+                <svg height="24" width="24">
+                    <title>Поиск</title>
+                    <use xlink:href="{{ asset('storage/img/icons.svg') }}#search"></use>
+                </svg>
+            </a>
+
+{{--            guest --}}
+            <a href="#">
+                <svg height="24" width="24">
+                    <title>Поиск</title>
+                    <use xlink:href="{{ asset('storage/img/icons.svg') }}#header-user"></use>
+                </svg>
+            </a>
+
+
+{{--            auth --}}
+            <a href="#">
+                <svg height="24" width="24">
+                    <title>Поиск</title>
+                    <use xlink:href="{{ asset('storage/img/icons.svg') }}#write"></use>
+                </svg>
+            </a>
+
+            <button class="btn px-0 py-0" type="button" data-toggle="dropdown" aria-expanded="false">
+                <img src="{{ asset('storage/img/user.svg') }}" alt="Профиль" title="Профиль">
+            </button>
+
+            <div class="dropdown-menu shadow mt-2" aria-labelledby="dropdownMenuButton" data-spy="scroll" data-offset="0">
+
+                <div class="profile-start-section d-flex align-items-center">
+                    <a href="#"><img src="{{ asset('storage/img/user.svg') }}" alt="Профиль" title="Профиль"></a>
+                    <a href="#">@profile-index</a>
+                </div>
+
+                <div class="dropdown-divider"></div>
+
+
+                <a class="dropdown-item" href="#">Статьи</a>
+                <a class="dropdown-item" href="#">Комментарии</a>
+
+                <div class="dropdown-divider"></div>
+
+                <a class="dropdown-item" href="#">Правила сайта</a>
+
+                <div class="dropdown-divider"></div>
+
+
+                <div class="profile-end-section">
+                    <a class="dropdown-item" href="#">
+                        <svg height="24" width="24" class="mr-2">
+                            <title>Поиск</title>
+                            <use xlink:href="{{ asset('storage/img/icons.svg') }}#settings"></use>
+                        </svg>
+                        <span>Настройки</span>
+                    </a>
+                    <a class="dropdown-item" href="#">
+                        <svg height="24" width="24" class="mr-2">
+                            <title>Поиск</title>
+                            <use xlink:href="{{ asset('storage/img/icons.svg') }}#logout"></use>
+                        </svg>
+                        <span>Выход</span>
+                    </a>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+</nav>
+
+<main class="d-flex align-content-center" style="min-height: 100vh">
+    <div class="container">
+        @yield('content')
+    </div>
+</main>
+
+
+<footer class="bg-dark text-white">
+    <div class="container d-flex align-content-between flex-row-reverse flex-wrap">
+        <div class="col-md d-flex d-md-none justify-content-center">
+            <a href="#" class="logo">GameInfo</a>
+        </div>
+        <div class="col-md d-flex justify-content-md-end justify-content-center">
+            <a class="" href="#">
+                <svg height="24" width="24" class="mx-2">
+                    <title>Поиск</title>
+                    <use xlink:href="{{ asset('storage/img/logos.svg') }}#social-logo-facebook"></use>
+                </svg>
+            </a>
+            <a class="" href="#">
+                <svg height="24" width="24" class="mx-2">
+                    <title>Поиск</title>
+                    <use xlink:href="{{ asset('storage/img/logos.svg') }}#social-logo-telegram"></use>
+                </svg>
+            </a>
+            <a class="" href="#">
+                <svg height="24" width="24" class="mx-2">
+                    <title>Поиск</title>
+                    <use xlink:href="{{ asset('storage/img/logos.svg') }}#social-logo-twitter"></use>
+                </svg>
+            </a>
+            <a class="" href="#">
+                <svg height="24" width="24" class="mx-2">
+                    <title>Поиск</title>
+                    <use xlink:href="{{ asset('storage/img/logos.svg') }}#social-logo-vkontakte"></use>
+                </svg>
+            </a>
+            <a class="" href="#">
+                <svg height="24" width="24" class="mx-2">
+                    <title>Поиск</title>
+                    <use xlink:href="{{ asset('storage/img/logos.svg') }}#social-logo-youtube"></use>
+                </svg>
+            </a>
+        </div>
+        <div class="col-md d-flex justify-content-md-start justify-content-center">© 2021–2022 «GameInfo»</div>
+    </div>
+</footer>
+
+<script src="{{ asset('js/app.js') }}"></script>
 
 </body>
 </html>
